a5436c8b05fe08f859ff5692a304c637fc2d246d
KukuJerung <khairulfilhan94@gmail.com>
2022-02-18 01:01:58 +0800

    components - map logic & style refactor

--- src/views/components/search-places/map.tsx
+++ src/views/components/search-places/map.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import useGetCoordinates from '../../../hooks/useGeolocation';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import MapView from 'react-native-maps';
 import { RootState } from '../../../state/ducks/places-search';
 import MapMarker from './marker';
+import tw from 'twrnc';
+import Icon from '../icon';
+import { useRef } from 'react';
+import { fetchCoordsSuccess } from '../../../state/ducks/places-search/actions';
 
 const Maps = () => {
     const baseCoords = useGetCoordinates();
+
+    const mapRef = useRef();
+    const dispatch = useDispatch();
     const { coords = [] } = useSelector(
         (state: RootState) => state.searchPlaces.coords
     );
     const coordsChanged = coords.length > 0;
+
+    const onMapCenter = () => {
+        dispatch(fetchCoordsSuccess(baseCoords));
+    };
+
     return (
-        <View style={styles.container}>
+        <View style={tw`h-auto`}>
             <MapView
-                style={styles.map}
+                style={tw`w-full h-full`}
                 zoomEnabled
                 zoomTapEnabled
                 minZoomLevel={8}
+                ref={mapRef}
                 region={{
                     latitude: coordsChanged
                         ? coords[0].geometry.location.lat
@@ -36,18 +49,18 @@ const Maps = () => {
                     coordsChanged={coordsChanged}
                 />
             </MapView>
+            <TouchableOpacity
+                onPress={onMapCenter}
+                style={tw`absolute self-end top-120 right-10 bg-white rounded-full p-1 `}
+            >
+                <Icon
+                    name={'locate'}
+                    style={tw`text-black text-4xl`}
+                    type="Ionicons"
+                />
+            </TouchableOpacity>
         </View>
     );
 };
 
 export default Maps;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
-    map: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height
-    }
-});
