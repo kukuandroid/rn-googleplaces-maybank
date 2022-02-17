5de4bb2b230c8874a2493e9dfbe3a3d2c7ebcb4c
KukuJerung <khairulfilhan94@gmail.com>
2022-02-17 23:46:17 +0800

    gmap api key change

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,3 @@
-import { google_maps_api_key } from 'react-native-dotenv';
 
 export const gmapPlacesAutoCompleteUrl =
     'https://maps.googleapis.com/maps/api/place/autocomplete/json?';
@@ -6,4 +5,4 @@ export const gmapGeocodingUrl =
     'https://maps.googleapis.com/maps/api/geocode/json?';
 export const proxyUrl = 'https://cors-anywhere.herokuapp.com';
 
-export const gmaps_api_key = "AIzaSyCsI9abmoQ86DoNEFmLDE7tHsaGxJsPE8Q";
+export const gmaps_api_key = "AIzaSyACuixL6nM4aQk9BGdz4v3LEjaXp8883kw";
