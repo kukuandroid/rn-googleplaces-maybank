dc062aae5db9d2cf6a0f30dc06c38dc0d1828d11
KukuJerung <khairulfilhan94@gmail.com>
2022-02-18 01:03:42 +0800

    screen - place search restyling tailwind & logic refactor

--- src/views/screens/PlacesSearchScreen.tsx
+++ src/views/screens/PlacesSearchScreen.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
-import { StyleSheet, FlatList } from 'react-native';
+import {
+    StyleSheet,
+    View as Views,
+    TouchableWithoutFeedback,
+    Keyboard,
+    FlatList
+} from 'react-native';
 
 import { View } from '../components/Themed';
 import { RootTabScreenProps } from '../../types';
@@ -9,7 +15,7 @@ import SearchResult from '../components/search-places/search-result';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCoords } from '../../state/ducks/places-search/actions';
 import { RootState } from '../../state/ducks/places-search';
-
+import tw from 'twrnc';
 export default function TabOneScreen({
     navigation
 }: RootTabScreenProps<'PlaceSearch'>) {
@@ -27,34 +33,28 @@ export default function TabOneScreen({
     };
 
     return (
-        <View style={styles.container}>
-            <Maps />
-            <SearchBar onSearch={onSearch} />
-            {showList && (
-                <FlatList
-                    contentContainerStyle={styles.flatlist}
-                    data={maps.places}
-                    renderItem={(item) => SearchResult(item, onAddressSelected)}
-                    keyExtractor={(item) => item.predictions}
-                    ItemSeparatorComponent={() => {
-                        return <View style={styles.separator} />;
-                    }}
-                />
-            )}
-        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+            <View>
+                <Maps />
+                <SearchBar onSearch={onSearch} />
+                {showList && (
+                    <Views
+                        style={tw`absolute mt-35 p-4 items-center justify-center self-center`}
+                    >
+                        <FlatList
+                            data={maps.places}
+                            contentContainerStyle={tw`bg-white rounded-lg p-1`}
+                            renderItem={(item) =>
+                                SearchResult(item, onAddressSelected)
+                            }
+                            keyExtractor={(item) => item.predictions}
+                            ItemSeparatorComponent={() => {
+                                return <View style={tw`h-0.2 bg-slate-300`} />;
+                            }}
+                        />
+                    </Views>
+                )}
+            </View>
+        </TouchableWithoutFeedback>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#fff'
-    },
-    flatlist: {
-        backgroundColor: '#fff',
-        marginTop: 10
-    },
-    separator: {
-        height: 1,
-        backgroundColor: '#CCCCCC'
-    }
-});
